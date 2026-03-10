254b5600b31b721bcc1c96190c2662b6c30fce45
Walter Stocker <walter.stocker@siemens-healthineers.com>
2026-03-10 09:08:57 +0100

    fix: dev-env on windows issues

--- dev-env/start.bat
+++ dev-env/start.bat
@@ -10,8 +10,8 @@ echo.
 echo Dev environment is up:
 echo   Traefik dashboard : http://localhost:8080/dashboard/
 echo   Traefik API       : http://localhost:8080/api/http/routers
-echo   app-alpha         : http://alpha.localhost
-echo   app-beta          : http://beta.localhost
-echo   app-gamma         : http://gamma.localhost
+echo   app-alpha         : http://alpha.localhost:8081/
+echo   app-beta          : http://beta.localhost:8081/
+echo   app-gamma         : http://gamma.localhost:8081/
 echo.
-echo To stop: docker compose -f dev-env\docker-compose.yml down
+echo To stop: docker compose -f "%SCRIPT_DIR%docker-compose.yml" down
